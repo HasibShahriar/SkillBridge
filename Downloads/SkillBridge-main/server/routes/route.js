23772379dd7ca0d511172a5c22b55e85f65e962e
Adnan <64307271+Adnann07@users.noreply.github.com>
2025-09-12 01:21:04 +0600

    Delete Downloads/SkillBridge-main/server directory

--- Downloads/SkillBridge-main/server/routes/route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { loginUser, newUser } from '../controllers/userController.js';
-
-const router = express.Router();
-
-router.post('/login',loginUser);
-router.post('/register',newUser);
-
-export default router;
